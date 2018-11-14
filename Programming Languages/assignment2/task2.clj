6f41aa9cdd041526f4c9c35ec079555cb687f8c7
Francis <francisjhackenberger@gmail.com>
2018-11-14 16:42:58 -0500

    CSCI 3055 - Clojure Assignment 2 Task 2, count top 10 instructors in 2015 sorted

---
+++ Programming Languages/assignment2/task2.clj
@@ -0,0 +1,49 @@
+#!/usr/bin/env clojure
+
+(require '[clojure.java.io :as io]
+		 '[clojure.string :as str])
+
+(defn list-2015 [filename]
+	(let [rdr (io/reader filename) output '()]
+	  (for [line (line-seq rdr)
+		:let [record (str/split line #";")]]
+        (and (= (re-find #"2015" (nth record 0)) "2015") (= (re-find #"CSCI" (nth record 3)) "CSCI")
+            (conj output (str (nth record 3) " " (nth record 1)))
+        )
+	  )
+	)
+)
+
+(defn get-instructors-2016 [filename]
+	(let [rdr (io/reader filename)]
+	  (for [line (line-seq rdr)
+		:let [record (str/split line #";")]
+        :when (and (not (= (nth record 19) "TBA"))
+                (and (= (re-find #"2016" (nth record 0)) "2016") 
+                (= (re-find #"Lecture" (nth record 18)) "Lecture")))]
+        (nth record 19)
+	  )
+	)
+)
+
+(defn most-frequent-n [n items]
+  (->> items
+    frequencies
+    (sort-by val)
+    reverse
+    (take n))
+)
+
+(let [output (distinct (list-2015 "data.csv"))]
+    (doseq [item output
+        :when (not (= item false))]
+            (apply println item))
+)
+
+(println " ")
+
+(let [instructors (get-instructors-2016 "data.csv")]
+    (doseq [instructor (most-frequent-n 10 instructors)]
+        (apply println instructor)
+    )
+)
