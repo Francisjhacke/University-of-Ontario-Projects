f32490aaf1aaa7af74df6d9321b8b3a769c02a72
Francis <francisjhackenberger@gmail.com>
2018-11-16 11:52:05 -0500

    Updated clojure scripts to use hashmaps rather than string maniupulation for output

--- Programming Languages/assignment2/task1.clj
+++ Programming Languages/assignment2/task1.clj
@@ -3,19 +3,20 @@
 (require '[clojure.java.io :as io]
 		 '[clojure.string :as str])
 
-(defn parsedata [filename]
-	(let [rdr (io/reader filename) output '()]
+(defn get-csci-courses [filename]
+	(let [rdr (io/reader filename)]
 	  (for [line (line-seq rdr)
 		:let [record (str/split line #";")]]
         (and (= (nth record 0) "201709") (= (re-find #"CSCI" (nth record 3)) "CSCI")
-            (conj output (str (nth record 3) " " (nth record 1)))
+            {:code (nth record 3) :name (nth record 1)}
         )
 	  )
 	)
 )
 
-(let [output (distinct (parsedata "data.csv"))]
-    (doseq [item output
+(let [courses (sort-by :code (distinct (get-csci-courses "2017-10-15.txt")))]
+    (doseq [item courses
         :when (not (= item false))]
-            (apply println item))
+            (println (get item :code) "" (get item :name))
+    )
 )
