6875b40dbdc9fba2e3d4799f4a6f4fb302603792
Francis <francisjhackenberger@gmail.com>
2018-11-29 10:29:55 -0500

    Assignment 3 completed and renamed csci 3055 projects

---
+++ Programming Languages/a3/csci3055u_a3/src/csci3055u_a3/core.clj
@@ -0,0 +1,40 @@
+(ns csci3055u_a3.core (:require [cheshire.core :refer :all]))
+
+
+(defn get-max [grades]
+  (reduce max (vec
+    (for [student (get grades :students)]
+      (get student :grade)
+    )))
+)
+
+(defn get-min [grades]
+  (reduce min (vec
+    (for [student (get grades :students)]
+      (get student :grade)
+    )))
+)
+
+(defn get-avg [grades]
+  (/ (reduce + (vec
+    (for [student (get grades :students)]
+      (get student :grade)
+    ))) (count (get grades :students)))
+)
+
+(defn get-best-student [grades]
+  (for [student (get grades :students) :when (== (get-max grades) (get student :grade))]
+    (get student :name)
+  )
+)
+
+(defn main [fileName]
+  (let [grades (parse-string (slurp fileName) true)]
+    (println (get grades :title))
+    (println "Range:" (format "%.2f" (Math/floor (get-min grades))) "to" (format "%.2f" (Math/floor (get-max grades))))
+    (println "Average:" (format "%.2f" (get-avg grades)))
+    (print "Best student: ")
+    (apply print (get-best-student grades))
+    (println "")
+  )
+)
